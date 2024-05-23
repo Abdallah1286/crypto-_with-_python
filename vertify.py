94ee0e3a1482cfe569d31185ad3ce48f89d95d70
Abdallah1286 <161259918+Abdallah1286@users.noreply.github.com>
2024-05-23 23:24:08 +0300

    Add files via upload

---
+++ vertify.py
@@ -0,0 +1,35 @@
+import sys
+from hashlib import *
+from Crypto.Util.number import *
+
+usage = "Usage:\n> python verify.py [firmware] [signature] [pubkey]"
+if len(sys.argv) < 2:
+    print(usage)
+    exit()
+
+FirmwareFile=sys.argv[1]
+with open(FirmwareFile,"rb") as file :
+    firmware_main = file.read()
+
+hash=sha256()
+hash.update(firmware_main)
+hash.digest()
+hash_firmware=bytes_to_long(hash.digest())
+
+sign_file= sys.argv[2]
+with open(sign_file,'rb') as file:
+    sign_file2=int(file.read().decode())
+
+pub_key= sys.argv[3]
+with open(pub_key,'rb') as file:
+    e=file.readline()
+    n=file.readline()
+e=int(e.decode())
+n=int(n.decode())
+calc_sign=pow(sign_file2,e,n)
+
+# verification
+if calc_sign == hash_firmware:
+    print("verified")
+else:
+    print("failed")
